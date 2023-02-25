d3a07222c6eb2116ac2ae738419f2e8857e523e8
bikrantnath22 <bikrantnath4@gmail.com>
2023-02-25 15:58:23 +0530

    login page design compelete

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
-
+import SideItem from "../Sidebar/SideItem";
 export default function Home() {
   return (
-    <div className='h-screen overflow-hidden'>JUST ANOTHER HOME COMPONENT</div>
+    <div className='h-screen overflow-hidden'>JUST ANOTHER HOME COMPONENT
+    
+    </div>
   )
 }
